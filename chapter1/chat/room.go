9cc238ccea92fbce05b1ef8afe3ae537ae6b22a9
mrmtsu <81202muramatsu@gmail.com>
2021-06-11 05:14:49 +0900

    http fundle

--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	forward chan []byte
 	join chan *client
@@ -11,8 +18,10 @@ func (r *room) run() {
 	for {
 		select {
 		case client := <-r.join:
+			// 参加
 			r.clients[client] = true
 		case client := <-r.leave:
+			// 退室
 			delete(r.clients, client)
 			close(client.send)
 		case msg := <-r.forward:
@@ -20,10 +29,35 @@ func (r *room) run() {
 				select {
 				case client.send <- msg:
 				default:
+					// 送信に失敗
 					delete(r.clients, client)
 					close(client.send)
 				}
 			}
 		}
 	}
+}
+
+const (
+	socketBufferSize = 1024
+	messageBuffersize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize:
+	socketBufferSize, WriteBufferSize: socketBufferSize}
+func (r *room) ServerHttp(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServerHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send: make(chan []byte, messageBuffersize),
+		room: r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client } ()
+	go client.write()
+	client.read()
 }
